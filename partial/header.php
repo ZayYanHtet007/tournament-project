1417d1fe1c1d9ff78272e23394e5d0169c614e38
kho <tnahktwook@gmail.com>
2025-12-16 12:40:41 +0630

    update file

--- partial/header.php
+++ partial/header.php
@@ -1,16 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
+<head>
 <meta charset="UTF-8">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="css/style.css">
     <title>Admin</title>
 <title>Game Tournament</title>
-<style ></style>
-<head>
+</head>
+<body>
+
 <header>
-    <div class="logo">GAME ARENA</div>
+    <div class="logo">TournaX</div>
     <nav>
         <a href="#">Home</a>
         <a href="#">Tournaments</a>
@@ -18,5 +20,3 @@
         <a href="#">Contact</a>
     </nav>
 </header>
-</head>
-<body>
