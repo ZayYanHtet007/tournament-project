45269052dedf7f214292cfad4f68acfa7755fdac
ZYAN <zayyan1817@gmail.com>
2025-12-16 12:34:12 +0630

    change

--- partial/header.php
+++ partial/header.php
@@ -2,21 +2,23 @@
 <html lang="en">
 <meta charset="UTF-8">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="../css/style.css">
-    <title>Admin</title>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+<link rel="stylesheet" href="../css/style.css">
+<title>Admin</title>
 <title>Game Tournament</title>
-<style ></style>
+<style></style>
+
 <head>
-<header>
-    <div class="logo">GAME ARENA</div>
-    <nav>
-        <a href="#">Home</a>
-        <a href="#">Tournaments</a>
-        <a href="#">Teams</a>
-        <a href="#">Contact</a>
-    </nav>
-</header>
+    <header>
+        <div class="logo">GAME ARENA</div>
+        <nav>
+            <a href="#">Home</a>
+            <a href="#">Tournaments</a>
+            <a href="#">Teams</a>
+            <a href="#">Contact</a>
+        </nav>
+    </header>
 </head>
+
 <body></body>
