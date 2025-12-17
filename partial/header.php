9a5363485f4382c93e4a37efaa1961bb2ab79681
ZYAN <zayyan1817@gmail.com>
2025-12-17 09:06:43 +0630

    changes

--- partial/header.php
+++ partial/header.php
@@ -6,15 +6,13 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="css/style.css">
     <title>Admin</title>
     <title>Game Tournament</title>
 </head>
 
 <body>
-    <?php
-    include('partial/footer.php');
-    ?>
+   
 
     <header>
         <div class="logo">TournaX</div>
@@ -25,5 +23,4 @@
             <a href="#">Contact</a>
         </nav>
     </header>
-    <?php include('database/dbConfig.php');
-    ?>
+    
